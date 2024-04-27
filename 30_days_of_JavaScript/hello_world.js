6f28001ee23f0ef27dd415df13a1511c27714dae
jdimawea <94670505+jdimawea@users.noreply.github.com>
2024-04-26 21:21:58 -0600

    hello

--- 30_days_of_JavaScript/hello_world.js
+++ 30_days_of_JavaScript/hello_world.js
@@ -1,8 +1,20 @@
-var createHelloWorld = function() {
+var merged = function(nums1, nums2, m, n) {
 
-    return function(...args) {
-        return "Hello World";
-    }
+    let nums3 = [];
+
+    let p = 0;
+   
+
 }
 
-console.log(createHelloWorld()());
+let nums1 = [1,2,3,0,0,0];
+let nums2 = [2,5,6];
+let m = 3;
+let n = 3;
+
+
+merged(nums1, nums2, m, n);
+
+console.log(nums3);
+
+
